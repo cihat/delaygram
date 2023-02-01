98c545bb8421c867b55b5828be51f4ab5e1c1f9c
Cihat Salik <cihatsalik1@gmail.com>
2023-02-01 19:22:07 +0300

    Add `SideBar` icons and css variables

--- frontend/src/components/Suggestion/index.tsx
+++ frontend/src/components/Suggestion/index.tsx
@@ -4,10 +4,16 @@ import styles from './Suggestion.module.scss'
 
 import cn from 'classnames'
 
+import useTheme from 'src/hooks/useTheme'
+
 const Suggestion: React.FC = () => {
+  const { theme, setTheme } = useTheme()
+  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
+
   return (
     <div className={cn(styles.container)}>
       <h1>Suggestion</h1>
+      <button onClick={toggleTheme}>Toggle Theme</button>
     </div>
   )
 }
