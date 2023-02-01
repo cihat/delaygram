d3062bbbf5560db450a08fdfe80950bb848f0f83
Cihat Salik <cihatsalik1@gmail.com>
2023-02-02 02:09:14 +0300

    Add sidebar icons

---
+++ frontend/src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+import HomeIcon from './home'
+import SearchIcon from './search'
+import VideoIcon from './video'
+import MessageIcon from './message'
+import InstagramIcon from './instagram-logo'
+
+export { HomeIcon, SearchIcon, VideoIcon, MessageIcon, InstagramIcon }
---
+++ frontend/src/assets/icons/search.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const LightSearchIcon = () => {
+  return (
+    <svg
+      aria-label="Explore"
+      color="var(--icon-color)"
+      fill="var(--icon-color)"
+      height="24"
+      role="img"
+      viewBox="0 0 24 24"
+      width="24"
+    >
+      <path
+        d="M19 10.5A8.5 8.5 0 1 1 10.5 2a8.5 8.5 0 0 1 8.5 8.5Z"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      ></path>
+      <line
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        x1="16.511"
+        x2="22"
+        y1="16.511"
+        y2="22"
+      ></line>
+    </svg>
+  )
+}
+
+export default LightSearchIcon
