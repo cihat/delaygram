fa679d4df174b3ad4fe750997cec170791483313
Cihat Salik <cihatsalik1@gmail.com>
2023-02-02 02:12:56 +0300

    Add jsx icons

--- frontend/src/components/SideBar/index.tsx
+++ frontend/src/components/SideBar/index.tsx
@@ -1,135 +1,38 @@
 import React from 'react'
 import styles from './SideBar.module.scss'
 
+import { HomeIcon, MessageIcon, SearchIcon, VideoIcon, InstagramIcon } from 'src/assets/icons'
+
 import cn from 'classnames'
 
 const SideBar: React.FC = () => {
   return (
     <div className={cn('', styles.container)}>
-      <div className={styles.icon_wrapper}>
-        <svg aria-label="Home" color="var(--text-color)" fill="var(--text-color)" height="24" role="img" viewBox="0 0 24 24" width="24">
-          <path d="M22 23h-6.001a1 1 0 0 1-1-1v-5.455a2.997 2.997 0 1 0-5.993 0V22a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V11.543a1.002 1.002 0 0 1 .31-.724l10-9.543a1.001 1.001 0 0 1 1.38 0l10 9.543a1.002 1.002 0 0 1 .31.724V22a1 1 0 0 1-1 1Z"></path>
-        </svg>
-        <svg aria-label="Explore" color="var(--text-color)" fill="var(--text-color)" height="24" role="img" viewBox="0 0 24 24" width="24">
-          <path
-            d="M19 10.5A8.5 8.5 0 1 1 10.5 2a8.5 8.5 0 0 1 8.5 8.5Z"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-          ></path>
-          <line
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            x1="16.511"
-            x2="22"
-            y1="16.511"
-            y2="22"
-          ></line>
-        </svg>
-        <svg aria-label="Reels" color="var(--text-color)" fill="var(--text-color)" height="24" role="img" viewBox="0 0 24 24" width="24">
-          <line
-            fill="none"
-            stroke="currentColor"
-            stroke-linejoin="round"
-            stroke-width="2"
-            x1="2.049"
-            x2="21.95"
-            y1="7.002"
-            y2="7.002"
-          ></line>
-          <line
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            x1="13.504"
-            x2="16.362"
-            y1="2.001"
-            y2="7.002"
-          ></line>
-          <line
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            x1="7.207"
-            x2="10.002"
-            y1="2.11"
-            y2="7.002"
-          ></line>
-          <path
-            d="M2 12.001v3.449c0 2.849.698 4.006 1.606 4.945.94.908 2.098 1.607 4.946 1.607h6.896c2.848 0 4.006-.699 4.946-1.607.908-.939 1.606-2.096 1.606-4.945V8.552c0-2.848-.698-4.006-1.606-4.945C19.454 2.699 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.546 2 5.704 2 8.552Z"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-          ></path>
-          <path
-            d="M9.763 17.664a.908.908 0 0 1-.454-.787V11.63a.909.909 0 0 1 1.364-.788l4.545 2.624a.909.909 0 0 1 0 1.575l-4.545 2.624a.91.91 0 0 1-.91 0Z"
-            fill-rule="evenodd"
-          ></path>
-        </svg>
-        <svg aria-label="New post" color="var(--text-color)" fill="var(--text-color)" height="24" role="img" viewBox="0 0 24 24" width="24">
-          <path
-            d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552Z"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-          ></path>
-          <line
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            x1="6.545"
-            x2="17.455"
-            y1="12.001"
-            y2="12.001"
-          ></line>
-          <line
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            x1="12.003"
-            x2="12.003"
-            y1="6.545"
-            y2="17.455"
-          ></line>
-        </svg>
-        <svg
-          aria-label="Messenger"
-          color="var(--text-color)"
-          fill="var(--text-color)"
-          height="24"
-          role="img"
-          viewBox="0 0 24 24"
-          width="24"
-        >
-          <path
-            d="M12.003 2.001a9.705 9.705 0 1 1 0 19.4 10.876 10.876 0 0 1-2.895-.384.798.798 0 0 0-.533.04l-1.984.876a.801.801 0 0 1-1.123-.708l-.054-1.78a.806.806 0 0 0-.27-.569 9.49 9.49 0 0 1-3.14-7.175 9.65 9.65 0 0 1 10-9.7Z"
-            fill="none"
-            stroke="currentColor"
-            stroke-miterlimit="10"
-            stroke-width="1.739"
-          ></path>
-          <path
-            d="M17.79 10.132a.659.659 0 0 0-.962-.873l-2.556 2.05a.63.63 0 0 1-.758.002L11.06 9.47a1.576 1.576 0 0 0-2.277.42l-2.567 3.98a.659.659 0 0 0 .961.875l2.556-2.049a.63.63 0 0 1 .759-.002l2.452 1.84a1.576 1.576 0 0 0 2.278-.42Z"
-            fill-rule="evenodd"
-          ></path>
-        </svg>
+      <div className={styles.logo_wrapper}>
+        <InstagramIcon />
+      </div>
+      {/* Need refactoring here(dynamic icons) */}
+      <div className={styles.icons_wrapper}>
+        <div className={styles.inner_icon_wrapper}>
+          <HomeIcon />
+          <h3>Home</h3>
+        </div>
+        <div className={styles.inner_icon_wrapper}>
+          <SearchIcon />
+          <h3>Search</h3>
+        </div>
+        <div className={styles.inner_icon_wrapper}>
+          <VideoIcon />
+          <h3>Reels</h3>
+        </div>
+        <div className={styles.inner_icon_wrapper}>
+          <MessageIcon />
+          <h3>Messages</h3>
+        </div>
+        <div className={styles.inner_icon_wrapper}>
+          <MessageIcon />
+          <h3>Notifications</h3>
+        </div>
         {/* <img
           alt="user profile picture"
           src="https://placeholder.com/assets/images/150x150-2-500x500.png"
