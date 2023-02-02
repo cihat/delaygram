5d7ebe747a848eafc6096d6978854a703e2a2cde
Cihat Salik <cihatsalik1@gmail.com>
2023-02-02 20:14:46 +0300

    Add fill icons

---
+++ frontend/src/assets/icons/search-fill.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable */
+
+import React from 'react'
+
+const SearchIcon = () => {
+  return (
+    <svg
+      aria-label="Search"
+      color="var(--icon-color)"
+      fill="var(--icon-color)"
+      height="24"
+      role="img"
+      viewBox="0 0 24 24"
+      width="24"
+    >
+      <path
+        d="M18.5 10.5a8 8 0 1 1-8-8 8 8 0 0 1 8 8Z"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="3"
+      ></path>
+      <line
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="3"
+        x1="16.511"
+        x2="21.643"
+        y1="16.511"
+        y2="21.643"
+      ></line>
+    </svg>
+  )
+}
+
+export default SearchIcon
